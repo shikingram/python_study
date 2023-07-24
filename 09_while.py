7115262e7d3473231a7ecc5a219dc203acb519a5
shikingram <kingram@163.com>
2023-07-24 15:10:22 +0800

    commit while for_range function

---
+++ 09_while.py
@@ -0,0 +1,17 @@
+i = 0
+while i < 10:
+    print("i love you")
+    i += 1
+
+"""
+i love you
+i love you
+i love you
+i love you
+i love you
+i love you
+i love you
+i love you
+i love you
+i love you
+"""
---
+++ 10_for_range.py
@@ -0,0 +1,47 @@
+name = "kingram"
+
+for x in name:
+    print(x)
+
+for x in range(10):
+    print(x)
+
+print("=============================")
+
+for x in range(5, 10):
+    print(x)
+
+print("=============================")
+
+for x in range(5, 10, 2):
+    if x == 9:
+        break
+    print(x)
+
+"""
+k
+i
+n
+g
+r
+a
+m
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+5
+6
+7
+8
+9
+5
+7
+9
+"""
